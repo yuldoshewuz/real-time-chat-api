e0a01b1909b178f2f7fe11169135a9390f3d22fb
yuldoshewuz <iamfozilbek@gmail.com>
2026-01-30 18:32:06 +0500

    Added routes for channels.

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('chat.{conversationId}', function ($user, $conversationId) {
+    return $user->conversations()->where('conversations.id', (int) $conversationId)->exists();
+});
+
+Broadcast::channel('online', function ($user) {
+    return [
+        'id' => $user->id,
+        'name' => $user->name
+    ];
+});
